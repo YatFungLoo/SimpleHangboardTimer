1c0711abff399ba6214002b9cecfb18b427ea719
YatFungLoo <looyatfung@gmail.com>
2025-04-12 02:13:42 +0800

    Initial Commit

---
+++ Simple Hangboard Timer/Simple_Hangboard_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Hangboard_TimerApp.swift
+//  Simple Hangboard Timer
+//
+//  Created by YatFungLoo on 12/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Hangboard_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Simple Hangboard TimerTests/Simple_Hangboard_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Hangboard_TimerTests.swift
+//  Simple Hangboard TimerTests
+//
+//  Created by YatFungLoo on 12/4/2025.
+//
+
+import Testing
+@testable import Simple_Hangboard_Timer
+
+struct Simple_Hangboard_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
