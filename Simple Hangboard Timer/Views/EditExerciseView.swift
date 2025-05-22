75b8f9307e9f0a21f29319c07ccbd2b25d7fe96f
YatFungLoo <looyafung@gmail.com>
2025-05-22 17:25:49 +0800

    temp: wheel picker for time

--- Simple Hangboard Timer/Views/EditExerciseView.swift
+++ Simple Hangboard Timer/Views/EditExerciseView.swift
@@ -10,13 +10,37 @@ import SwiftUI
 struct EditExerciseView: View {
     @Binding var exercise: Exercise
     
+    private let minRange = 0..<6
+    private let secRange = 0..<60
+    
     var body: some View {
         Form {
+            Section(header: Text("Title")) {
+                TextField("Title", text: $exercise.title)
+            }
             Section(header: Text("Training Details")) {
                 HStack {
-                    Label("On", systemImage: "carrot.fill")
-                    Spacer()
-                    Text("\(exercise.secOrMin(lengthInSeconds: exercise.intervals[0].hang))")
+                    VStack {
+                        HStack {
+                            Label("Off", systemImage: "carrot")
+                            Spacer()
+                            Text("\(exercise.intervals[0].hang)")
+                        }
+                        HStack {
+                            Picker(selection: $exercise.intervals[0].hangMinIndex, label: Text("Hang")) {
+                                ForEach(minRange, id: \.self) {     // TODO: look into this more soon.
+                                    Text("\($0)")
+                                }
+                            }
+                            .pickerStyle(WheelPickerStyle())
+                            Picker(selection: $exercise.intervals[0].hangSecIndex, label: Text("Hang")) {
+                                ForEach(secRange, id: \.self) { 
+                                    Text("\($0)")
+                                }
+                            }
+                            .pickerStyle(WheelPickerStyle())
+                        }
+                    }
                 }
                 HStack {
                     Label("Off", systemImage: "carrot")
